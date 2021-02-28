4f419335f9897b5d27c9d172aede9ab96d8687db
Younes Cheikh <younes.cheikh@gmail.com>
2021-02-28 22:48:07 +0100

    Updated author and url links

--- Source/Umator.Plugins.Files/PluginAssemblyInfo.cs
+++ Source/Umator.Plugins.Files/PluginAssemblyInfo.cs
@@ -3,6 +3,7 @@ using Umator.Contract;
 
 [assembly: UmatorPlugin(
     "7C805178-B48F-49D5-84BE-DF316F120140",
-    "Umator Files Plugin",
-    "Plugin for files management in a local or remote files system", Author = "Younes Cheikh"
+    "Umator.Files",
+    "Plugin for files management in a local or remote files system", Author = "Umator", AuthorUri = "https://umator.com/",
+    DocumentationUri = "https://umator.com/docs/umator-files/", SupportUri = "https://umator.com/support/forum/?view=forum&id=11"
     )]
