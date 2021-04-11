a70da62e14f2d4ec29bc618ff319f700c70415af
Younes CHEIKH <younes.cheikh@gmail.com>
2021-04-11 20:01:16 +0200

    version 1.0.3

--- Source/Routindo.Plugins.Files/PluginAssemblyInfo.cs
+++ Source/Routindo.Plugins.Files/PluginAssemblyInfo.cs
@@ -4,5 +4,5 @@
     "7C805178-B48F-49D5-84BE-DF316F120140",
     "Files Management",
     "Plugin for files management and monitoring in a local or remote files system", Author = "Routindo", AuthorUri = "https://routindo.com/files-management/",
-    DocumentationUri = "https://routindo.com/docs/files-management/", SupportUri = "https://routindo.com/support/forum/?view=forum&id=11"
+    DocumentationUri = "https://routindo.com/docs/files-management/", SupportUri = "https://routindo.com/support/forum/?view=forum&id=8"
     )]
