43e0caeee1d679019dd0f9c135579fbe078e34c6
Younes CHEIKH <younes.cheikh@gmail.com>
2021-05-12 05:32:22 +0200

    Added Argument Info on Result Files Argument

--- Source/Routindo.Plugins.Files.Components/Watchers/FilesWatcherResultArguments.cs
+++ Source/Routindo.Plugins.Files.Components/Watchers/FilesWatcherResultArguments.cs
@@ -1,7 +1,12 @@
-﻿namespace Routindo.Plugins.Files.Components.Watchers
+﻿using System.Collections.Generic;
+using Routindo.Contract.Attributes;
+
+namespace Routindo.Plugins.Files.Components.Watchers
 {
     public static class FilesWatcherResultArguments
     {
+        [ArgumentInfo("Files Collection", true, typeof(List<string>),
+            "List of Files sorted basing on the criteria defined in the Watcher Configuration")]
         public const string Files = nameof(Files);
     }
 }
